48cdec175fbaa226c62de0c14fbc6ba8c7c0f749
BalyshevArtem <bal429@yandex.ru>
2018-10-02 18:54:20 +0300

    new split

---
+++ split.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#define Max_size 100
+
+void Split(char* string,char* delimiters,char*** tokens,int* tokensCount){
+	
+	int len = strlen(string);
+	(*tokensCount) = 0;
+	(*tokens)[*tokensCount] = strtok(string, delimiters);
+	while((*tokens)[*tokensCount] != NULL){
+		(*tokensCount)++;
+		(*tokens)[*tokensCount] = strtok(NULL, delimiters);
+	}
+
+}
+
+int main(){
+	int i = 0;
+	char string[Max_size] = {};
+	int tokensCount = 0;
+	char delimiters[2] = {'\t','\n'};
+	fgets(string,Max_size,stdin);
+	char **tokens;
+	tokens = (char**)malloc(Max_size * sizeof(char*));
+	for(i = 0; i < Max_size; i++){
+		tokens[i] = (char*)malloc(Max_size * sizeof(char));
+	}
+	Split(string,delimiters,&tokens,&tokensCount);
+	for (i = 0; i < tokensCount; i++){
+        	printf("%s\n", tokens[i]);
+        }
+	free(tokens);
+	return 0;
+}
+
+
+
