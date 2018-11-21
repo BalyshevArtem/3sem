ad3d3707d18193265663198adc2b5ce007695487
BalyshevArtem <bal429@yandex.ru>
2018-11-21 19:16:10 +0300

    New threads

--- speed.c
+++ speed.c
@@ -1,27 +1,18 @@
-//Для N = 3 ускорение составило ~8,7 
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
+#define NMAX 1000
 
-/*
-У вас, как я понял, квадратные матрицы со стороной (N * N + 1) и вы их перемножаете с помощью N нитей.
-Чтобы честно измерить ускорение нужно:
-1) взять пару матриц большого размера, например, 1000 * 1000
-2) Число нитей K варьировать от 1 до числа процессоров на вашем компьютере, т.е. до 4 или 8, при этом 
-размеры матриц не меняются.
-3) на всякий случай удостовериться, что при фиксированных входных матрицах результирующее произведение от числа нитей K не зависит.
-*/
-
-int** Makematrix(int** mat, int n){
-	mat = (int**)calloc(n * n + 1, sizeof(int));
+int** Makematrix(int** mat){
+	mat = (int**)calloc(NMAX * NMAX, sizeof(int));
 	int i = 0;
 	int j = 0;
-	for(i; i < n; i++){
-		mat[i] = (int*)calloc(n * n + 1, sizeof(int));
+	for(i; i < NMAX; i++){
+		mat[i] = (int*)calloc(NMAX * NMAX, sizeof(int));
 	} 
-	for (i = 0; i < n; i++){
-		for(j = 0; j < n; j++){
-			mat[i][j] = rand() % 100;
+	for (i = 0; i < NMAX; i++){
+		for(j = 0; j < NMAX; j++){
+			mat[i][j] = rand() % 10;
 		}
 	}
 	return mat;	
@@ -31,14 +22,14 @@ struct Matrix{
 	int** second;
 	int** result;
 	int counter;
-	int constN;
+	int del;
 };
 
-void PrintMatrix(int** mat, int n){
+void PrintMatrix(int** mat){
 	int i = 0;
 	int j = 0;
-	for(i = 0; i < n; i++){
-		for(j = 0; j < n; j++){
+	for(i = 0; i < NMAX; i++){
+		for(j = 0; j < NMAX; j++){
 			printf("%d  ", mat[i][j]);
 		}
 		printf("\n");
@@ -48,13 +39,15 @@ void PrintMatrix(int** mat, int n){
 
 void* MatrixMult(void* dummy){
 	struct Matrix mat = *((struct Matrix*)dummy);
-	int i = 0;
 	int j = 0;
+	int i;
 	int k = 0;
-	for(j = 0; j < mat.constN; j++){
-		mat.result[mat.counter][j] = 0;
-		for(k = 0; k < mat.constN; k++){
-			mat.result[mat.counter][j] += (mat.first[mat.counter][k] * mat.second[k][j]);  
+	for(i = 0; i < mat.counter; i++){
+		for(j = 0; j < NMAX; j++){
+			mat.result[i + mat.del][j] = 0;
+			for(k = 0; k < NMAX; k++){
+				mat.result[i + mat.del][j] += (mat.first[i + mat.del][k] * mat.second[k][j]);  
+			}
 		}
 	}
 	return NULL;
@@ -69,32 +62,34 @@ int main(){
 	int** matrix2;
 	int** matrix_result;
 	int i = 0;
-	matrix_result = (int**)calloc(N * N + 1, sizeof(int));
-	for(i = 0; i < N; i++){
-		matrix_result[i] = (int*)calloc(N * N + 1, sizeof(int)); 
+	matrix_result = (int**)calloc(NMAX * NMAX, sizeof(int));
+	for(i = 0; i < NMAX; i++){
+		matrix_result[i] = (int*)calloc(NMAX * NMAX, sizeof(int)); 
 	} 
 	struct Matrix Mat;
-	Mat.first = Makematrix(matrix1, N);
-	Mat.second = Makematrix(matrix2, N);
+	Mat.first = Makematrix(matrix1);
+	Mat.second = Makematrix(matrix2);
 	Mat.result = matrix_result;
-	PrintMatrix(Mat.first, N);
+	PrintMatrix(Mat.first);
 	printf("\n\n\n");
-	PrintMatrix(Mat.second, N);
-	Mat.constN = N;
+	PrintMatrix(Mat.second);
+	Mat.del = 0;
+	Mat.counter = 0;
+	Mat.counter = NMAX / N;
 	for (i = 0; i < N; i++){
 		pthread_t thid;
 		int result;
-		Mat.counter = i;
 		result = pthread_create(&thid,(pthread_attr_t *)NULL, MatrixMult, &Mat);
 		if( result != 0){
 			printf("ERROR with Create threads");
 			exit(-1);
 		}
-	pthread_join(thid, (void **) NULL);		
+	pthread_join(thid, (void **) NULL);
+	Mat.del += NMAX / N;		
 	}
 	printf("\n\n\n");
-	PrintMatrix(Mat.result, N);
-	for(i = 0; i < N; i++){
+	PrintMatrix(Mat.result);
+	for(i = 0; i < NMAX; i++){
 		free(Mat.first[i]);
 		free(Mat.second[i]);
 		free(Mat.result[i]);
